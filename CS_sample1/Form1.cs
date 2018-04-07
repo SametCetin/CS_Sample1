8d376ece8a4d8e3b4b6220b6c78228ac59cfafe1
SametCetin <sametcetin@windowslive.com>
2018-04-07 11:20:55 +0300

    Started

---
+++ CS_sample1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_sample1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            dr = MessageBox.Show("Are you sure", "Attention", MessageBoxButtons.OKCancel);
+
+            if (dr == DialogResult.OK)
+            {
+                MessageBox.Show("OK", "Result");
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                MessageBox.Show("Cancel", "Result");
+            }
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# CS_Sample1
